2fdde1af4321a26a88069b0a0a1d997cd5dbdb51
Lucas Gomes <lucasgomes.exe@gmail.com>
2026-03-23 15:37:15 -0400

    fix: refactor stores query to use geometry object

--- src/main/kotlin/eti/lucasgomes/makalu/features/stores/StoreRepository.kt
+++ src/main/kotlin/eti/lucasgomes/makalu/features/stores/StoreRepository.kt
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query
 
 interface StoreRepository : JpaRepository<StoreEntity, Long> {
     @Query(
-        value = "SELECT * FROM stores WHERE ST_DWithin(location, ST_SetSRID(ST_MakePoint(?1, ?2), 4326), ?3)",
+        value = "SELECT * FROM stores WHERE ST_DWithin(location::geography, ST_SetSRID(ST_MakePoint(?1, ?2), 4326)::geography, ?3)",
         nativeQuery = true
     )
     fun findWithinDistance(longitude: Double, latitude: Double, distanceInMeters: Double): List<StoreEntity>
